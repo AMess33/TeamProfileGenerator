a01dcc0eb6bc80c9c0ef69e110443d25cbfc5e09
AMess33 <amess3309@gmail.com>
2023-02-22 13:47:40 -0600

    complete intern test file and intern js file. all tests passing

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,28 @@
 const Intern = require ("../lib/Intern")
+
+describe( "Intern", () => {
+    const intern = new Intern('John', 'john@yahoo.com', 'Kansas');
+    
+    it("should get a name from Employee", () => {
+        expect(intern.getName()).toEqual('John')
+        });
+    
+        it("should get an ID from intern", () => {
+        expect(intern.getId()).toEqual(1)
+        });
+    
+        it("should get an email from intern", () => {
+        expect(intern.getEmail()).toEqual('john@yahoo.com');
+        });
+    
+        it("should get a role from intern and assign it to intern", () => {
+        expect(intern.getRole()).toEqual('Intern');
+        });
+
+        it("should get the interns school", () => {
+            expect(intern.getSchool()).toEqual('Kansas');
+        });
+
+
+
+});
